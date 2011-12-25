8230b69ac4f3cfd3e02fb5adbfaecb745189877f
Cestmir Houska <czestmyr@gmail.com>
2011-12-25 17:30:45 +0100

    TripleOscillator file cleanup
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- synths/triosc.cpp
+++ synths/triosc.cpp
@@ -4,6 +4,33 @@
 
 using namespace std;
 
+TripleOscillator::TripleOscillator(int bufsize):
+	_bufsize(bufsize),
+	_mixer(bufsize),
+	_adsr(),
+	_first(0.0),
+	_first_obs(this, PROP_FIRST),
+	_second(0.0),
+	_second_obs(this, PROP_SECOND),
+	_shift(0),
+	_shift_obs(this, PROP_SHIFT),
+	_first_gen(GEN_SINE),
+	_second_gen(GEN_SINE),
+	_third_gen(GEN_SINE),
+	_gen_obs(this, PROP_GEN) {
+	for (int i = 0; i < POLYPHONY; ++i) {
+		_adsr[i] = new Adsr(20, 100, 101, 2000);
+		_buffers[i] = new float[bufsize];
+		for (int j = 0; j < 3; ++j) {
+			_generators[i][j] = GeneratorFactory::inst()->createGenerator(GEN_SINE, 440.0, 0);
+		}
+	}
+
+	for (int i = 0; i < POLYPHONY; ++i) {
+		_channelQueue.push(i);
+	}
+}
+
 void TripleOscillator::setGenerator(int oscId, IGenerator* gen) {
 	if (oscId < 0 || oscId >= 3) return;
 	for (int i = 0; i < POLYPHONY; ++i) {
--- synths/triosc.h
+++ synths/triosc.h
@@ -24,33 +24,7 @@ enum {
 
 class TripleOscillator: public ISynth{
 	public:
-		TripleOscillator(int bufsize):
-			_bufsize(bufsize),
-			_mixer(bufsize),
-			_adsr(),
-			_first(0.0),
-			_first_obs(this, PROP_FIRST),
-			_second(0.0),
-			_second_obs(this, PROP_SECOND),
-			_shift(0),
-			_shift_obs(this, PROP_SHIFT),
-			_first_gen(GEN_SINE),
-			_second_gen(GEN_SINE),
-			_third_gen(GEN_SINE),
-			_gen_obs(this, PROP_GEN) {
-			for (int i = 0; i < POLYPHONY; ++i) {
-				_adsr[i] = new Adsr(20, 100, 101, 2000);
-				_buffers[i] = new float[bufsize];
-				for (int j = 0; j < 3; ++j) {
-					_generators[i][j] = GeneratorFactory::inst()->createGenerator(GEN_SINE, 440.0, 0);
-				}
-			}
-
-			for (int i = 0; i < POLYPHONY; ++i) {
-				_channelQueue.push(i);
-			}
-		}
-
+		TripleOscillator(int bufsize);
 		~TripleOscillator() {
 			for (int i = 0; i < POLYPHONY; ++i) {
 				for (int j = 0; j < 3; ++j) {
