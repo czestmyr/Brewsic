b19cf7fefe7a2949d5dc685766afd421af021091
Cestmir Houska <czestmyr@gmail.com>
2012-09-03 20:11:03 +0200

    Indentation fix
    Signed-off-by: Cestmir Houska <czestmyr@gmail.com>

--- synths/Isynth.h
+++ synths/Isynth.h
@@ -5,23 +5,23 @@
 #include "common/signals.h"
 
 class ISynth {
-	public:
-		ISynth(): _createGui(this) {}
-                virtual ~ISynth() {}
+  public:
+    ISynth(): _createGui(this) {}
+    virtual ~ISynth() {}
 
-		virtual void startNote(int noteId, float frequency) = 0;
-		virtual void stopNote(int noteId) = 0;
+    virtual void startNote(int noteId, float frequency) = 0;
+    virtual void stopNote(int noteId) = 0;
 
-		virtual void generateOutput() = 0;
-		virtual float* getBuffer() = 0;
+    virtual void generateOutput() = 0;
+    virtual float* getBuffer() = 0;
 
-		virtual const char* getClassName() = 0;
+    virtual const char* getClassName() = 0;
 
-		void setGuiParent(SafePtr<IControl> parent) { _gui_parent = parent; }
-		SIGNAL_DESTINATION(_createGui, ISynth, createGui);
-		virtual void createGui() = 0;
-	protected:
-		SafePtr<IControl> _gui_parent;
+    void setGuiParent(SafePtr<IControl> parent) { _gui_parent = parent; }
+    SIGNAL_DESTINATION(_createGui, ISynth, createGui);
+    virtual void createGui() = 0;
+  protected:
+    SafePtr<IControl> _gui_parent;
 };
 
 #endif
